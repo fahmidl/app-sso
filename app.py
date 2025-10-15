bc820787e4edc012c4cc97c153abd15bdf6f9f83
fahmidl <fahmidyan@gmail.com>
2025-10-15 11:58:40 +0700

    feat: Add ProxyFix to handle reverse proxy headers

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ load_dotenv()
 
 # Inisialisasi aplikasi Flask
 app = Flask(__name__)
+app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1)
 app.secret_key = os.urandom(24)
 
 # --- Konfigurasi Database ---
